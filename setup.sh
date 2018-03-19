e995c9bd8cc322140ccd580e4d0de9bb131c6c7f
Kezun Sima <simakezun@outlook.com>
2018-03-19 00:56:53 -0700

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #/bin/bash
 
 dir=$(dirname "$0")
+fullPath=$PWD
+if [ "$dir" != "." -a "$dir" != "" ]; then
+  fullPath=$fullPath/$dir
+fi
 for dotfile in gitignore ctags bashrc gitconfig inputrc template.cpp tmux.conf vimrc;
 do
-  ln -f -s $PWD/$dir/$dotfile $HOME/.$dotfile
+  ln -f -s $fullPath/$dotfile $HOME/.$dotfile
 done
