667c3d2b035741b74851af5c0eeb3375cb7e6caf
Kezun Sima <simakezun@outlook.com>
2019-01-02 23:25:54 +0000

    update

--- setup.sh
+++ setup.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+maybeCreateDir() {
+  dir=$1
+  if [ ! -r ${dir} ]; then
+    echo "Creating ${dir}"
+    mkdir -p ${dir}
+  fi
+}
+
 dir=$(dirname "$0")
 fullPath=${PWD}
 if [ "${dir}" != "." -a "${dir}" != "" ]; then
@@ -11,8 +19,7 @@ if [ -r ${HOME}/.dotfiles ]; then
   rm -i ${HOME}/.dotfiles
 fi
 ln -s ${fullPath} ${HOME}/.dotfiles
-for dotfile in clang-format gitignore ctags bashrc gitconfig inputrc template.cpp tmux.conf vimrc;
-do
+for dotfile in clang-format gitignore ctags bashrc gitconfig inputrc template.cpp tmux.conf vimrc; do
   if [ -r ${HOME}/.${dotfile} ]; then
     echo "${HOME}/.${dotfile} already exists! Skipped it."
   else
@@ -20,11 +27,16 @@ do
   fi
 done
 
+maybeCreateDir ${HOME}/.vim
+
 for vimfile in filetype.vim; do
   ln -f -s ${fullPath}/${vimfile} ${HOME}/.vim/${vimfile}
 done
 
 echo "Installing Tmux Plugin Manager..."
+
+maybeCreateDir ${HOME}/tmux/plugins
+
 tmuxPluginDir=${HOME}/tmux/plugins
 if [ ! -r ${tmuxPluginDir} ]; then
   mkdir -p ${tmuxPluginDir}
