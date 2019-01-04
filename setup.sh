4165af3321584a883948cb0b66fbe4c24efa78e9
Kezun Sima <simakezun@outlook.com>
2019-01-04 15:07:23 -0800

    Fix bugs

--- setup.sh
+++ setup.sh
@@ -10,27 +10,25 @@ maybeCreateDir() {
   fi
 }
 
-dir=$(dirname "$0")
-fullPath=${PWD}
-if [ "${dir}" != "." -a "${dir}" != "" ]; then
-  fullPath=${fullPath}/${dir}
-fi
+progname=$(basename "$0")
+dirname=$(cd "$(dirname "$0")" || exit 1; pwd -P)
+
 if [ -r ${HOME}/.dotfiles ]; then
   rm -i ${HOME}/.dotfiles
 fi
-ln -s ${fullPath} ${HOME}/.dotfiles
+ln -s ${dirname} ${HOME}/.dotfiles
 for dotfile in clang-format gitignore ctags bashrc gitconfig inputrc template.cpp tmux.conf vimrc; do
   if [ -r ${HOME}/.${dotfile} ]; then
     echo "${HOME}/.${dotfile} already exists! Skipped it."
   else
-    ln -f -s ${fullPath}/${dotfile} ${HOME}/.${dotfile}
+    ln -f -s ${dirname}/${dotfile} ${HOME}/.${dotfile}
   fi
 done
 
 maybeCreateDir ${HOME}/.vim
 
 for vimfile in filetype.vim; do
-  ln -f -s ${fullPath}/${vimfile} ${HOME}/.vim/${vimfile}
+  ln -f -s ${dirname}/${vimfile} ${HOME}/.vim/${vimfile}
 done
 
 echo "Installing Tmux Plugin Manager..."
